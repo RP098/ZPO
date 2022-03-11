d47f54c73138da9730112d080ec49f97c73c4406
Adam <adam98gorski@gmail.com>
2022-03-11 15:49:31 +0100

    zmiany

---
+++ WzorceProjektowe/Podatki/WielkaBrytania.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podatki
+{
+    public class WielkaBrytania : IPodatekDochodowy
+    {
+        public decimal podatekDochodowyOblicz(decimal Kwota) => Kwota * 0.1M+10;
+    }
+}
