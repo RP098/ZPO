a42468b1e2fabfc047ddf06d328d631870333e54
Adam <44466509+RP098@users.noreply.github.com>
2022-05-04 19:52:02 +0200

    Singleton

---
+++ Zad4_Wspolbiezny/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad4_Wspolbiezny
+{
+    public class Singleton
+    {
+        static Singleton singleton;
+        private Singleton()
+        {
+            
+        }
+        public int ZwiekszLiczbe { get; set; } = 0;
+
+       public static  Singleton  GetInstance( )
+        {
+            if (Singleton.singleton == null)
+                Singleton.singleton = new Singleton();
+            lock (singleton)
+            {
+                if (Singleton.singleton == null)
+                {
+                    Singleton.singleton = new Singleton();
+                    return Singleton.singleton;
+                }
+                else
+                {
+                    return Singleton.singleton;
+                }
+            }
+        }
+
+    }
+}
