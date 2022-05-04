0c1654a9e0343d6e12cde7003f3e839574bf2ddd
Adam <44466509+RP098@users.noreply.github.com>
2022-05-04 18:48:54 +0200

    Obs

---
+++ Obserwator/Obserwator/ZmianaLiczba.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obserwator
+{
+    class ZmianaLiczba : ILiczba
+    {
+        List<Liczby> liczby = new List<Liczby>();
+        public void dodajLiczbe(Liczby liczba)
+        {
+            liczby.Add(liczba);
+            if (liczba.liczba == 0)
+                powiadomienie(liczba.liczba, "równa zero");
+            else
+                usunLiczbe(liczba);
+        }
+
+        public void powiadomienie(int liczba, string tekst)
+        {
+            foreach (var item in liczby)
+            {
+                item.update(liczba, tekst);
+            }
+        }
+
+
+        public void usunLiczbe(Liczby liczba)
+        {
+            liczby.Remove(liczba);
+
+        }
+
+       
+    }
+}
