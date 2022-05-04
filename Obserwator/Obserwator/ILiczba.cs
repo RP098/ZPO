12e8372043c037797dd8c4dee1098436ab0723e7
Adam <44466509+RP098@users.noreply.github.com>
2022-05-04 18:47:13 +0200

    Obserwator

---
+++ Obserwator/Obserwator/ILiczba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obserwator
+{
+    interface ILiczba
+    {
+        void dodajLiczbe(Liczby liczba);
+        void usunLiczbe(Liczby liczba);
+        void powiadomienie(int liczba, string tekst);
+        
+    }
+}
